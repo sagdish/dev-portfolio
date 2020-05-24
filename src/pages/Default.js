8c9cfe6b8e8c748dda9eae6aa8550c7abf07d92e
sagdish <forsi1@yahoo.com>
2020-05-24 00:25:10 -0700

    slider done

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+import image1 from '../img/one.png'
+import image2 from '../img/two.png'
+import image3 from '../img/three.png'
 
 function Default(props) {
   return (
@@ -15,9 +20,36 @@ function Default(props) {
       </div>
 
       <div className='content-carousel'>
-        <div>
+        <CarouselProvider
+          naturalSlideWidth={16}
+          naturalSlideHeight={9}
+          isPlaying={true}
+          totalSlides={3}
+        >
+          <div className='carousel'>
+            <Slider className='slider'>
+              <Slide index={0}>
+                <Image src={image1}/>
+              </Slide>
+              <Slide index={1}>
+                <Image src={image2}/>
+              </Slide>
+              <Slide index={2}>
+                <Image src={image3}/>
+              </Slide>
+            </Slider>
+
+            <div className='buttons'>
+              <ButtonBack style={{fontSize:20}}>&lt;</ButtonBack>
+              <ButtonNext style={{fontSize:20}}>&gt;</ButtonNext>
+            </div>
+          </div>
+
+        </CarouselProvider>
+
+        {/* <div>
           carousel
-        </div>
+        </div> */}
       </div>
 
         <div className='content-carousel_line'>
