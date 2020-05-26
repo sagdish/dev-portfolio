4094e75680b1212537bebd02d8c33c07f0b04cde
sagdish <forsi1@yahoo.com>
2020-05-26 00:01:11 -0700

    slider buttons, font awesome, icons ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Footer(props) {
   return (
     <div className='footer'>
       <div className='footer-social'>
-        social media icons
+        <a href="https://github.com/sagdish" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        >
+          <FontAwesomeIcon icon={faGithub} size='3x' className='icon'/>
+        </a>
+
+        <a href="https://linkedin.com/in/sagdi-formanov/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        >
+          <FontAwesomeIcon icon={faLinkedin} size='3x' className='icon'/>
+        </a>
+
+        <a href="https://twitter.com/FormanovSagdi" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        >
+          <FontAwesomeIcon icon={faTwitter} size='3x' className='icon' />
+        </a>
+
+        <a href="https://instagram.com/forsi_ph" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        >
+          <FontAwesomeIcon icon={faInstagram} size='3x' className='icon'/>
+        </a>
+
       </div>
       <div className='footer-name'>
         SAGDI FORMANOV
--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image } from 'pure-react-carousel';
+import React, { useState } from 'react';
+import { CarouselProvider, 
+  Slider, Slide, ButtonBack, 
+  ButtonNext, Image
+} from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
 
 import image1 from '../img/one.png'
 import image2 from '../img/two.png'
 import image3 from '../img/three.png'
 
-function Default(props) {
+function Default() {
+
+  const [animate, setAnimate] = useState(false);
+  // console.log(animate)
+
+  const addAndRemove = () =>{
+    setAnimate(true)
+    console.log(animate)
+    setTimeout(() => {
+      setAnimate(false)
+      console.log(animate)
+    }, 1000);
+  }
+
   return (
     <div className='content'>
       <div className='content-title'>
@@ -22,37 +40,55 @@ function Default(props) {
       <div className='content-carousel'>
         <CarouselProvider
           naturalSlideWidth={16}
-          naturalSlideHeight={9}
+          naturalSlideHeight={9.5}
           isPlaying={true}
           totalSlides={3}
         >
+          {/* <h1 className="projectsName">Projects:</h1> */}
           <div className='carousel'>
             <Slider className='slider'>
               <Slide index={0}>
-                <Image src={image1}/>
+                <a href="https://arcregistry.com" 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                >
+                  <Image src={image1}/>
+                </a>
               </Slide>
               <Slide index={1}>
-                <Image src={image2}/>
+                <a href="https://sf-spots.sagdi.now.sh/" 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                >
+                  <Image src={image2}/>
+                </a>
               </Slide>
               <Slide index={2}>
-                <Image src={image3}/>
+                <a href="https://lyfe-logger-fe.herokuapp.com/" 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                >
+                  <Image src={image3}/>
+                </a>
               </Slide>
             </Slider>
-
-            <div className='buttons'>
-              <ButtonBack style={{fontSize:20}}>&lt;</ButtonBack>
-              <ButtonNext style={{fontSize:20}}>&gt;</ButtonNext>
-            </div>
+            <ButtonBack className='slider-buttons_l' 
+              onClick={addAndRemove}
+            >
+              <FontAwesomeIcon icon={faArrowAltCircleLeft} size='lg' />
+            </ButtonBack>
+            <ButtonNext className='slider-buttons_r'
+              onClick={addAndRemove}
+            >
+              <FontAwesomeIcon icon={faArrowAltCircleRight} size='lg' />
+            </ButtonNext>
           </div>
 
         </CarouselProvider>
-
-        {/* <div>
-          carousel
-        </div> */}
       </div>
 
-        <div className='content-carousel_line'>
+        <div className={`content-carousel_line ${animate? 'animate' : ''}`}>
+        {/* <div className='content-carousel_line animate'> */}
         </div>
     </div>
   );
