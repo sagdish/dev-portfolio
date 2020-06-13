0c0a6e14ed8030f83198510516eb5b00b73fb9bc
sagdish <forsi1@yahoo.com>
2020-06-12 22:21:30 -0700

    stoic quotes useless rerender fix

--- src/components/Stoic.js
+++ src/components/Stoic.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSpring, animated as a } from 'react-spring';
 
 import './style.css'
@@ -6,8 +6,11 @@ import { quotes } from './quotes';
 // import { index } from './Carousel';
 
 function Stoic(props) {
-  const {quotesList, setQuotesList} = useState(quotes)
-  const [index, setIndex] = useState(1)
+  const [quotesList, setQuotesList] = useState([])
+  const [random, setRandom] = useState( Math.floor( Math.random() * (quotes.length)) );
+  useEffect(()=> {
+    setQuotesList(quotes)
+  },[])
   const [flipped, setFlip] = useState(false)
   const { transform, opacity } = useSpring({
     opacity: flipped ? 1 : 0,
@@ -15,32 +18,30 @@ function Stoic(props) {
     config: { mass: 5, tension: 500, friction: 110 }
   });
   // let i = 1;
-  const random = Math.floor( (Math.random() * (quotes.length - index)) + 1);
-  const replace = quotes[(quotes.length - 1) - index]
+  // const replace = quotes[(quotes.length - 1) - index]
 
   const randomize = () => {
-    setFlip(state => !state)
+    setFlip(state => !state);
+    setRandom( Math.floor(Math.random() * (quotes.length)) )
     // setQuotesList([...quotesList.slice(0, random -1), replace, ...quotesList.slice(random)  ])
-    quotes[random] = replace
-    index < quotes.length ? setIndex(index + 1) : setIndex(1)
+    // quotes[random] = replace
+    // index < quotes.length ? setIndex(index + 1) : setIndex(1)
   }
-
-  console.log('r:', random, 'i:',index)
-  console.log(quotes)
+  console.log(quotes.length, random)
   return (
     <div onClick={randomize}>
       <a.div 
         className='card'
         style= {{ opacity: opacity.interpolate(o => 1 - o), transform }}
       >
-        {quotes[random]}
+        {quotesList[random]}
       </a.div>
 
       <a.div 
         className='card'
         style= {{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}
       >
-         {random > 1 ? quotes[random-1] : quotes[random] }
+         {quotesList[random] }
       </a.div>
     </div>
   );
