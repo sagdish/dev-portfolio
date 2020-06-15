eb6924a6cb3e8a75867b364cc2bc78cb4c0a9ee0
sagdish <forsi1@yahoo.com>
2020-06-14 19:52:36 -0700

    Blog section

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import Navbar from './components/Navbar';
 import Default from './pages/Default';
 import About from './pages/About';
 import Work from './pages/Work';
+import Blog from './pages/Blog';
 import Footer from './components/Footer';
 import './App.css';
 
 function App(prop) {
   return (
     <div className='main-container'>
-          <Navbar />
-            <Fade
-              delay={1000}
-              duration={3000}
-            >
-              <Route exact path='/' component={ Default } />
-              <Route path='/about' component={ About } />
-              <Route path='/projects' component={ Work } />
-
-            </Fade>
-          <Footer />
-        </div>
+      <Navbar />
+        <Fade delay={1000} duration={3000}>
+          <Route exact path='/' component={ Default } />
+          <Route path='/about' component={ About } />
+          <Route path='/projects' component={ Work } />
+          <Route path='/blog' component={ Blog } />
+        </Fade>
+      <Footer />
+    </div>
   );
 }
 
