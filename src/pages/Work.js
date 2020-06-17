6b9bdefb2f01311db777af084843dfb994434ebb
sagdish <forsi1@yahoo.com>
2020-06-16 20:55:56 -0700

    about page layout fix

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -4,32 +4,34 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 
+import './style.css'
 import '../App.css'
 
 function Work(props) {
   return (
     <Fade duration={2000} >
-    <div className='content'>
-      <div>
-        <Link to='/' >
-          <FontAwesomeIcon icon={faChevronLeft} size='3x' className='back-btn' />
-        </Link>
-      </div>
-      <div className='content-title' style={{
-        // height: '40vh',
-        // width: 'auto',
-        border: '1px solid white',
-      }}>
-        In Progress...
-      </div>
+      <div className='pages-content'>
 
-      <div className='content-title' style={{
-        border: '1px solid white'
-      }}>
-     
+        <div className='back-btn'>
+          <Link to='/' style={{
+            color: '#DADADA'
+          }}>
+            <FontAwesomeIcon icon={faChevronLeft} size='3x' />
+          </Link>
+        </div>
+    
+        <div className='content-title'>           
+         placeholder
+        </div>
+
+        <div className='content-title' style={{
+          // fontSize: '23px',
+          // border: '1px solid white'
+        }}>
+          placeholder
+        </div>
+        
       </div>
-      
-    </div>
     </Fade>
   );
 }
