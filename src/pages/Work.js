8bc9b9c6c5485689b8ec4aefec82b2d5acbefb4c
sagdish <forsi1@yahoo.com>
2020-06-16 22:44:12 -0700

    projects work

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -4,6 +4,7 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 
+import sfspot from '../img/MainLogo.png'
 import './style.css'
 import '../App.css'
 
@@ -12,34 +13,40 @@ function Work(props) {
     <Fade duration={2000} >
       <div className='pages-content'>
 
-        <div className='back-btn'>
+        <div className='back-btn-blog'>
           <Link to='/' style={{
-            color: '#DADADA'
+            color: '#DADADA',
           }}>
             <FontAwesomeIcon icon={faChevronLeft} size='3x' />
           </Link>
         </div>
     
-        <div className='content-title'>
-          <div className="box">
-
-            SF Study Spots
-          </div>
+        <div className='box'>
+        <img src={sfspot} alt='' style={{
+            height: 'auto',
+            width: '70%',
+            // border: '1px solid white',
+            // borderRadius: '5%',
+            // boxShadow: '7px 13px 29px -8px rgba(0,0,0,0.75)',
+            alignSelf: 'center'
+          }}/>
+            {/* SF Study Spots */}
+        
         </div>
 
-        <div className='content-title' style={{
+        <div className='box' style={{
           // fontSize: '23px',
           // border: '1px solid white'
         }}>
           LifeLogger
         </div>
-        <div className='content-title' style={{
+        <div className='box' style={{
           // fontSize: '23px',
           // border: '1px solid white'
         }}>
           GraphQL server
         </div>
-        <div className='content-title' style={{
+        <div className='box' style={{
           // fontSize: '23px',
           // border: '1px solid white'
         }}>
