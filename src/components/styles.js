9dea716f60cad3281e1227d28eef808f0dd45977
sagdish <forsi1@yahoo.com>
2020-08-15 23:40:38 -0700

    contact form visual

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,5 +1,5 @@
 import { animated } from 'react-spring'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const Container = styled(animated.div)`
   display: grid;
@@ -41,4 +41,87 @@ const Item = styled(animated.div)`
   }
 `
 
-export { Container, Item }
+const sharedStyles = css`
+  background-color: #eee;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  margin: 10px 0 15px 0;
+  padding: 10px;
+  box-sizing: border-box;
+`
+const StyledFormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 600px;
+  padding: 0 20px;
+  font-weight: lighter;
+  font-size: 16px;
+`
+const StyledForm = styled.form`
+  width: 100%;
+  max-width: 700px;
+  padding: 40px;
+  background-color: #212224;
+  border-radius: 10px;
+  box-sizing: border-box;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+  /* font-weight: lighter; */
+`
+const StyledInput = styled.input`
+  display: 'block';
+  width: 100%;
+  ${sharedStyles}
+`
+const StyledTextArea = styled.textarea`
+  background-color: #eee;
+  width: 100%;
+  min-height: 100px;
+  resize: none;
+  ${sharedStyles}
+`
+const StyledButton = styled.button`
+  display: block;
+  background-color: #343538;
+  color: #fff;
+  font-size: 18px;
+  border: 0;
+  border-radius: 5px;
+  height: 40px;
+  padding: 0 20px;
+  cursor: pointer;
+  box-sizing: border-box;
+`
+
+const StyledFieldset = styled.fieldset`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 20px 0;
+
+  legend {
+    padding: 0 10px;
+  }
+
+  label {
+    padding-right: 20px;
+  }
+
+  input {
+    margin-right: 10px;
+  }
+
+`
+const StyledError = styled.div`
+  color: red;
+  font-weight: 500;
+  margin: 0 0 40px 0;
+`
+
+
+export { 
+  Container, Item, StyledFormWrapper,
+  StyledForm, StyledInput, StyledTextArea,
+  StyledButton, StyledFieldset, StyledError
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,6 +9,11 @@ import Tooltip from 'react-tooltip-lite';
 import mail_box from '../img/mail_box.png'
 import './style.css'
 import '../App.css'
+import {
+  StyledFormWrapper,
+  StyledForm, StyledInput, StyledTextArea,
+  StyledButton, StyledFieldset, StyledError
+} from '../components/styles.js';
 
 function Contact(props) {
 
@@ -35,16 +40,16 @@ function Contact(props) {
           }}>
           <div style={{
             // position: 'absolute',
-            top: '26%',
-            left: '25%',
+            // top: '26%',
+            // left: '25%',
             fontSize: '26px',
             fontWeight: 'lighter',
             lineHeight: '1.8',
             marginBottom: '20px'
           }}>
-           Best to reach me at
+           You can reach me at
            <br/>
-           my email
+           my email or contact form here
             <Tooltip
               content='copied'
               eventOn='onClick'
@@ -54,28 +59,55 @@ function Contact(props) {
               color='white'
               direction='left'
             >
-            <CopyToClipboard text={email1 + email2}>
-              <button style={{
-                backgroundColor: '#212224',
-                color: '#d4d4d4',
-                width: '120px',
-                height: '30px',
-                border: '1px dashed #d4d4d4',
-                borderRadius: '7px',
-                cursor: 'pointer',
-              }}>Copy to clipboard</button>
-           </CopyToClipboard>
+              <CopyToClipboard text={email1 + email2}>
+                <button style={{
+                  backgroundColor: '#212224',
+                  color: '#d4d4d4',
+                  width: '120px',
+                  height: '30px',
+                  border: '1px dashed #d4d4d4',
+                  borderRadius: '7px',
+                  cursor: 'pointer',
+                }}>Copy to clipboard</button>
+              </CopyToClipboard>
             </Tooltip>
 
-           <br/>
-           <small>or Linkedin <FontAwesomeIcon icon={faArrowDown} size='sm' /></small>
+           {/* <br/>
+           <small>or Linkedin <FontAwesomeIcon icon={faArrowDown} size='sm' /></small> */}
           </div>
-          <img src={mail_box} alt='' style={{
+          {/* contact form goes here */}
+          <StyledFormWrapper>
+            <StyledForm>
+              <p style={{ fontSize: '23px' }}>Contact me</p>
+              <label htmlFor="name">Name</label>
+              <StyledInput type='text' name="name" />
+              <label htmlFor='email'>Email</label>
+              <StyledInput type="email" name="email" />
+              <StyledFieldset>
+                <legend>2 + 2 * 2 = ?</legend>
+                <label>
+                  <input type='radio' value='6' name='quiz' />
+                  6
+                </label>
+                <label>
+                  <input type='radio' value='8' name='quiz' />
+                  8
+                </label>
+              </StyledFieldset>
+              <label htmlFor='message'>Message</label>
+              <StyledTextArea name='message' />
+              <StyledError><p>Error message here</p></StyledError>
+              <StyledButton type='submit'>Send Me</StyledButton>
+            </StyledForm>
+          </StyledFormWrapper>
+          {/* end of contact form */}
+
+          {/* <img src={mail_box} alt='' style={{
               height: 'auto',
               width: '40%',
               alignSelf: 'center',
               marginLeft: '10%'
-            }}/>
+            }}/> */}
         </div>
       </div>
     </Fade>
