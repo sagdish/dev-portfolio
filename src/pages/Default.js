c50683c0dd0f2421c10e6abbf5c975458bd4c4c9
Sagdi Formanov <forsi1@yahoo.com>
2022-10-24 13:56:28 -0700

    Update Default.js

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -78,7 +78,7 @@ function Default() {
     <div className='content'>
 
       <div className='content-title'>
-        <p>Full Stack Developer <br/> Experimenting with UI/UX </p>
+        <p>Product Manager <br/> Designer & Developer <br/> In the past </p>
         <Container className={`content-title_button ${open ? '' : 'glow'}`} style={{ ...rest, width: width, height: height }} onClick={() => set(open => !open)}>
           {transition.length === 0 ? '. . .' : ''}
           {transition.map(({ item, key, props }, i) => (
