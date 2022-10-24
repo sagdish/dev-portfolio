0efac4416758d03fefa3a8a17a043e89606a61d2
Sagdi Formanov <forsi1@yahoo.com>
2022-10-24 14:02:11 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -47,7 +47,7 @@ function About(props) {
           }}
         >
           Hey, I’m Sagdi.
-          I am Web Developer with Automobile Engineering background. I like to create new stuff and experiment with it. 
+          Product Manager with Automobile Engineering background. I transitioned into the Product world from Development and Design fields. <br/> I like to create new stuff and experiment with it. 
           Nothing is constant, so I favor action over perfection (I learn it the hard way). I am passionate about User Experience and Design, 
           I’m in the process to nail that skill on professional level. This website is one of my attempts of trial and error in my learning process.
           I hope you like it.
